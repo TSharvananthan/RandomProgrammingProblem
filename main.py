0e3622927ac2bacc6b776a0a32b2c4cc346dae5a
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-03 17:41:03 -0400

    completed implementation, need to do small fixes

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from bs4 import BeautifulSoup
-from webbrowser import open_new_tab
-from requests import get
+try:
+    from bs4 import BeautifulSoup
+    import argparse
+    from webbrowser import open_new_tab
+    from requests import get
+    from time import sleep
+    from tqdm import trange
+    import sys
+    import argparse
+    import src.getLinks as gL
+    from random import choice
+except ImportError:
+    raise ImportError("Installing all required libraries for you")
+    os.system("pip3 install bs4")
+    os.system("pip3 install requests")
+    os.system("pip3 install tqdm")
+    os.system("pip3 install argparse")
 
 args_object = argparse.ArgumentParser()
-args_object.add_argument("update", type=int, default=0)
+args_object.add_argument("--update", type=int, default=0, help="Would you like to update your links text file? Enter 1 to update your links file")
+args = args_object.parse_args()
 
-args = args_object.parser_args()
+if args.update == 1: gL.UpdateLinks()
+elif not args.update == 1 and not args.update == 0: raise ValueError("Input Either 0 or 1")
 
-link_list = open("links.txt", "w")
+links = open("src/links.txt", "r").read().split("\n")
+
+print(choice(links))
