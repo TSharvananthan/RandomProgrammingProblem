799cc944c71a830a8e5fb89e33ff8445e8102028
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-03 19:52:40 -0400

    removed import try clause, added new argument

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-try:
-    from bs4 import BeautifulSoup
-    import argparse
-    from webbrowser import open_new_tab
-    from requests import get
-    from time import sleep
-    from tqdm import trange
-    import sys
-    import argparse
-    import src.getLinks as gL
-    from random import choice
-except ImportError:
-    raise ImportError("Installing all required libraries for you")
-    os.system("pip3 install bs4")
-    os.system("pip3 install requests")
-    os.system("pip3 install tqdm")
-    os.system("pip3 install argparse")
+from bs4 import BeautifulSoup
+import argparse
+from webbrowser import open_new_tab
+from requests import get
+from time import sleep
+from tqdm import trange
+import sys
+import argparse
+import src.getLinks as gL
+from random import choice
 
 args_object = argparse.ArgumentParser()
 args_object.add_argument("--problems", type=int, default=1, help="How many problems would you like?")
