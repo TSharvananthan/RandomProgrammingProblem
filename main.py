1232dd679592d21ba1de61277742ea808e1de349
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-03 18:12:49 -0400

    small fixes to main

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ except ImportError:
     os.system("pip3 install argparse")
 
 args_object = argparse.ArgumentParser()
-args_object.add_argument("--update", type=int, default=0, help="Would you like to update your links text file? Enter 1 to update your links file")
+args_object.add_argument("--problems", type=int, default=1, help="How many problems would you like?")
+args_object.add_argument("--update", type=bool, default=False, help="Would you like to update your links text file? Enter 1 to update your links file")
 args = args_object.parse_args()
 
-if args.update == 1: gL.UpdateLinks()
+if args.update == 1: gL.UpdateLinks(open("src/links.txt", "w"))
 elif not args.update == 1 and not args.update == 0: raise ValueError("Input Either 0 or 1")
 
 links = open("src/links.txt", "r").read().split("\n")
 
-print(choice(links))
+for x in range(args.problems):
+    chosen = choice(links)
+    open_new_tab(chosen)
